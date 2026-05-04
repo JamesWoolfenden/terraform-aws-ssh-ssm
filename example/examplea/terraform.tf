284e5974f7b9a5967b47293d6f69cf7fe0ef5651
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:50:02 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
-    aws = {
-      version = "4.31.0"
-      source  = "hashicorp/aws"
-    }
     tls = {
       version = "2.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.43.0"
     }
   }
   required_version = ">=0.14.8"
